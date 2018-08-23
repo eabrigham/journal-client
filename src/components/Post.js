8a21a26820709fd8a0f910d0cc42f90fdc8546fd
eabrigham <eabrigham@gmail.com>
2018-08-23 14:19:24 -0400

    Styled Post and app background color.

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../styles/Post.css'
 
 // need to create a post with title, content, date
 // need to pull data from app which pulls from database
 // via ajax
 const Post = props => {
     return (
-        <div>
+        <div className="post">
             <h3>{props.post ? props.post.title : null}</h3>
             {/* want to have post title and content, eventually date */}
             <p>{props.post.content}</p>
